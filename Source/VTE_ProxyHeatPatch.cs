f8dee4f4f8f0bb19ff1ace31c8c91b4e85de2963
ewolfe093 <ewolfe.seidr@gmail.com>
2025-03-06 12:34:49 -0800

    repatched RecalculateAffectedCells, converted to Postfix patches

--- Source/VTE_ProxyHeatPatch.cs
+++ Source/VTE_ProxyHeatPatch.cs
@@ -3,6 +3,8 @@ using Verse;
 using System.Collections.Generic;
 using System.Linq;
 using ProxyHeat;
+using System;
+using System.Reflection;
 
 namespace VTE_ProxyHeatPatch
 {
@@ -20,47 +22,31 @@ namespace VTE_ProxyHeatPatch
     [HarmonyPatch(typeof(ProxyHeatManager), "GetTemperatureOutcomeFor")]
     public static class ProxyHeatManager_GetTemperatureOutcomeFor_Patch
     {
-        public static bool Prefix(IntVec3 cell, float result, ref float __result, Map ___map, ProxyHeatManager __instance)
+        public static void Postfix(IntVec3 cell, ref float __result, ProxyHeatManager __instance)
         {
-            if (cell.Impassable(___map))
-            {
-                __result = result;
-                return false;
-            }
-
             if (__instance.temperatureSources.TryGetValue(cell, out List<CompTemperatureSource> tempSources))
             {
-                if (!cell.UsesOutdoorTemperature(___map))
-                {
-                    foreach (var comp in tempSources)
-                    {
-                        __instance.MarkDirty(comp);
-                    }
-                    __result = result;
-                    return false;
-                }
-                
                 var tempResults = new List<float>();
                 foreach (var tempSourceCandidate in tempSources)
                 {
-                    var tempResult = result;
+                    var tempResult = __result;
                     var props = tempSourceCandidate.Props;
                     var tempOutcome = tempSourceCandidate.TemperatureOutcome;
                     if (tempOutcome != 0)
                     {
-                        if (!(props.maxTemperature.HasValue && result >= props.maxTemperature.Value && tempOutcome > 0) &&
-                            !(props.minTemperature.HasValue && props.minTemperature.Value >= result && tempOutcome < 0))
+                        if (!(props.maxTemperature.HasValue && __result >= props.maxTemperature.Value && tempOutcome > 0) &&
+                            !(props.minTemperature.HasValue && props.minTemperature.Value >= __result && tempOutcome < 0))
                         {
                             tempResult += tempOutcome;
 
                             if (props.maxTemperature.HasValue)
                             {
-                                tempResult = System.Math.Min(tempResult, props.maxTemperature.Value);
+                                tempResult = Math.Min(tempResult, props.maxTemperature.Value);
                             }
 
                             if (props.minTemperature.HasValue)
                             {
-                                tempResult = System.Math.Max(tempResult, props.minTemperature.Value);
+                                tempResult = Math.Max(tempResult, props.minTemperature.Value);
                             }
                         }
                     }
@@ -68,15 +54,34 @@ namespace VTE_ProxyHeatPatch
                 }
                 if (tempResults.Count > 1)
                 {
-                    result = tempResults.Average();
+                    __result = tempResults.Average();
                 }
                 else if (tempResults.Count == 1)
                 {
-                    result = tempResults[0];
+                    __result = tempResults[0];
                 }
-                __result = result;
             }
-            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CompTemperatureSource), "RecalculateAffectedCells")]
+    public static class CompTemperatureSource_RecalculateAffectedCells_Patch
+    {
+        private static readonly FieldInfo affectedCellsField = typeof(CompTemperatureSource).GetField("affectedCells", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly FieldInfo affectedCellsListField = typeof(CompTemperatureSource).GetField("affectedCellsList", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly FieldInfo mapField = typeof(CompTemperatureSource).GetField("map", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static void Postfix(CompTemperatureSource __instance)
+        {
+            HashSet<IntVec3> affectedCells = (HashSet<IntVec3>)affectedCellsField.GetValue(__instance);
+            List<IntVec3> affectedCellsList = (List<IntVec3>)affectedCellsListField.GetValue(__instance);
+            Map map = (Map)mapField.GetValue(__instance);
+
+            affectedCells.RemoveWhere(cell => cell.GetEdifice(map) != null);
+            affectedCellsList.RemoveAll(cell => cell.GetEdifice(map) != null);
+
+            affectedCellsList.Clear();
+            affectedCellsList.AddRange(affectedCells);
         }
     }
 
